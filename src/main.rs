5e12f0e7bf19936fba33456ac5371ac31e6f9011
dearfl <i@flr.me>
2024-12-10 14:39:22 +0800

    abstract systems into plugin

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,38 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Alice".to_string())));
-    commands.spawn((Person, Name("Bob".to_string())));
-    commands.spawn((Person, Name("Charles".to_string())));
-}
+pub struct GreetPlugin;
 
-fn greet_people(people: Query<&Name, With<Person>>) {
-    for name in people.iter() {
-        println!("Hello {}!", name.0);
+impl GreetPlugin {
+    fn add_people(mut commands: Commands) {
+        commands.spawn((Person, Name("Alice".to_string())));
+        commands.spawn((Person, Name("Bob".to_string())));
+        commands.spawn((Person, Name("Charles".to_string())));
+    }
+
+    fn greet_people(people: Query<&Name, With<Person>>) {
+        for name in people.iter() {
+            println!("Hello {}!", name.0);
+        }
+    }
+
+    fn update_people(mut people: Query<&mut Name, With<Person>>) {
+        for mut name in people.iter_mut() {
+            name.0.make_ascii_uppercase();
+        }
     }
 }
 
-fn update_people(mut people: Query<&mut Name, With<Person>>) {
-    for mut name in people.iter_mut() {
-        name.0.make_ascii_uppercase();
+impl Plugin for GreetPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, Self::add_people)
+            .add_systems(Update, (Self::update_people, Self::greet_people).chain());
     }
 }
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (update_people, greet_people).chain())
+        .add_plugins(DefaultPlugins)
+        .add_plugins(GreetPlugin)
         .run();
 }
